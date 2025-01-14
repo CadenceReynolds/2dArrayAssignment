2ef026e86e1d671768519a8c7375eced0c99f47e
CadenceReynolds <reynocal001@warren.k12.in.us>
2025-01-14 13:31:59 -0500

    Finished Project

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+
+    public static void main(String[] args) {
+        int[][] array =
+                {
+                        {1, 2, 3},
+                        {77, 88, 25},
+                        {4, 120, -6}
+                };
+
+        for (int[] row : array) {
+            int rowSum = 0;
+            for (int value : row) {
+                rowSum += value;
+            }
+            System.out.println(rowSum);
+        }
+
+        for (int[] col : array) {
+            int colSum = 0;
+            for (int value : col) {
+                colSum += value;
+            }
+            System.out.println(colSum);
+        }
+    }
+
+
 }
